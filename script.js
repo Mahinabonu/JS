c987de90cf306377ee5b63b5e35a2849278db722
MAHINA <mahinabekova@gmail.com>
2022-04-05 13:10:19 +0500

    1st zadachi

---
+++ script.js
@@ -0,0 +1,105 @@
+//data
+
+let a=3;
+let b=6;
+let c=9;
+const pi= 3.14;
+
+// BEGIN 1
+
+// let a = 3 //+prompt("message")//+ before "prompt" changes string to num
+let p = 4 * a
+console.log(p)
+
+//2
+
+let s = a ** 2
+console.log(s)
+
+//3
+
+let s1 = a * b;
+let p1 = 2 * (a + b)
+console.log(p1, s1)
+
+//4
+
+let d = 2;
+let l = pi * d;
+console.log(l);
+
+//5
+
+let v = a ** 3;
+let s2 = 6 * a ** 2;
+console.log(v, s2)
+
+//6
+
+let v1 = a * b * c;
+let s3=2*(a*b+b*c+a*c);
+console.log(v1,s3);
+
+//7
+
+let r=3;
+let l1=2*pi*r;
+let s4=pi*r**2;
+console.log(l1,s4)
+
+//8
+
+let ave=(a+b)/2;
+console.log(ave);
+
+//9 correct?
+let sqr= Math.sqrt(a*b)
+console.log(sqr)
+
+let sqrd=(a*b)**(1/2);
+console.log(sqrd)
+
+//10
+let q=a**2+b**2;
+let w=c**2/a**2;
+let e=a**2*c**2;
+let t=b**2-a**2;
+console.log(q,w,e,t)
+
+//11  correct?
+let y=Math.abs(-3);
+let u = Math.abs(-6);
+let i=u/y;
+let o=y*u;
+let j=y+u;
+let k=u-y
+console.log(i,o,j, k)
+
+//12 correct?
+
+ // let P= a+b+c;
+// console.log(gip, P)
+
+//13
+let R1=16;
+let R2=10;
+let S1=pi*(R1)**2;
+let S2=pi*(R2)**2;
+let S3=S1-S2;
+console.log(S3);
+
+//14
+//let R=5;
+let R=(S/pi)(1/2)
+let L=2*pi*R;
+let S=pi*R**2;
+// let L1=pi*D;
+console.log(S,R);
+
+
+// //15
+// let
+
+
+
+
